22da2e09fc3584ed36ab1e11eb0beb94e7d95c25
JoaoPaulo-creator <joaosarto@gmail.com>
2024-08-22 21:49:39 -0300

    f

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-clang++ -std=c++2b -Wall -Wextra -pedantic -O2 toy.cpp -o myprogram
+#clang++ -std=c++2b -Wall -Wextra -pedantic -O2 toy.cpp -o myprogram
+clang++ -g toy.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o toy
